bd311ff9507ff2fb9678a3de5ddc722f30db2fab
kubikoss <kuba.posvic@email.cz>
2025-01-23 23:05:13 +0100

    .

--- Assets/Script/Managers/InventoryManager.cs
+++ Assets/Script/Managers/InventoryManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using TMPro;
 using Unity.VisualScripting;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
